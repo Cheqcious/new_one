7403a6fd0ca3364f5082e9cedb2e219b0bf3faf0
Cheqcious <qiguoxuan12138@gmail.com>
2024-12-16 18:50:32 +0800

    WeatherInfo.js

--- src/components/WeatherInfo.js
+++ src/components/WeatherInfo.js
@@ -84,9 +84,9 @@ const WeatherInfo = () => {
       {/* 第一行：天气信息 */}
       <Row>
         <WeatherIcon>⛅</WeatherIcon>
-        <TempText>11°C  </TempText>
+        <TempText>8°C  </TempText>
         <div><WeatherDescription>  多云（实时）</WeatherDescription></div>
-        <div><WeatherDescription>全天温度</WeatherDescription> <TempText1>3-15°C</TempText1></div>
+        <div><WeatherDescription>全天温度</WeatherDescription> <TempText1>-3-9°C</TempText1></div>
         <div><WeatherDescription>西南风 3-4级</WeatherDescription></div>
       </Row>
 
